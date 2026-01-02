250d893e2b215fa1c66ed1320788289a85a4bdc3
haileyphham <148396555+haileyphham@users.noreply.github.com>
2026-01-02 00:24:56 -0800

    Time: 150 ms (7.58%) | Memory: 26.3 MB (89.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        curr_min = float(inf)
+        curr_min = float(inf) #nothing is greater than this 1st price will be curr min
         max_profit = 0
 
-        for i in range(len(prices)):
-            curr_min = min(prices[i], curr_min)
-            max_profit = max(max_profit, prices[i] - curr_min)
+        for price in prices:
+            curr_min = min(curr_min, price) #want to find the smallest min as we iterate; keep it
+            max_profit = max(max_profit, price-curr_min) #finding the max profit 
         return max_profit
-
