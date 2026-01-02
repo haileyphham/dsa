866b3b1f1217a12ef7482ca0da30b068258366f5
haileyphham <148396555+haileyphham@users.noreply.github.com>
2026-01-01 22:29:13 -0800

    Time: 1 ms (57.00%) | Memory: 18.6 MB (66.08%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,18 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #index through the nums array
-        #remainder = target - nums[i] 
-        #if remainder in seen, return [seen[remainder],nums]
-        #if not add to seen and move on
 
         seen = {}
+
         for i in range(len(nums)):
             remainder = target - nums[i]
             if remainder in seen:
-                return [seen[remainder], i]
-            else: 
+                return [i, seen[remainder]]
+            else:
                 seen[nums[i]] = i
-
-
-        
-        
+            
