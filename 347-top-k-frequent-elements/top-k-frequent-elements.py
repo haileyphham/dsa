59648f9af4dc47d922d9c30e3a160038aa581676
haileyphham <148396555+haileyphham@users.noreply.github.com>
2026-01-03 14:28:50 -0800

    Time: 3 ms (91.40%) | Memory: 20.9 MB (81.39%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #loop through the array
-        #if in dict, up the count
-        #if not, add to dict and put count as 1
-        #sort the list of the dictionary keys by its values
-        # slice the list by k
-        #return the list
-        dict = {}
+        ''' o(n) time complexity, go through the array and add if seen in dict, up the frequency, if not,add it to the set'''
 
-        for i in range(len(nums)):
-            if nums[i] in dict:
-                dict[nums[i]] += 1
-            else:
-                dict[nums[i]] = 1
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1 # if in dict will return value, else: none
+        heap = []
+        for num, count in freq.items(): #num iterates through the key and count iterates through the value
+            heapq.heappush(heap, (count,num)) #pushes elem onto minheap orders by count (if count same then by num) 
+            if len(heap)>k:
+                heapq.heappop(heap) 
+                #at this point, the heap contains the top k elements as a tuple (num,count)
+        return [num for count, num in heap]
 
-        sorted_items = sorted(dict.items(), key=lambda x: x[1], reverse=True)
 
-        return [key for key, value in sorted_items[:k]]
 
-        
