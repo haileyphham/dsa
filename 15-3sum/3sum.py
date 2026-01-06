cd8545fd69defccd5d4a82a92f7479d54e94ab17
haileyphham <148396555+haileyphham@users.noreply.github.com>
2026-01-05 20:08:07 -0800

    Time: 589 ms (49.61%) | Memory: 20.2 MB (75.31%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,17 +4,15 @@ class Solution:
         output = []
 
         for i in range(len(nums)):
-    
-            if i>0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i] == nums[i-1]: #check i duplicates
                 continue
-
             l = i+1
-            r = len(nums)-1
-            while l<r:
-                total = nums[i] + nums[l] + nums[r]
+            r = len(nums) - 1
             
+            while l<r:
+                total = nums[i]+nums[l]+nums[r]
                 if total == 0:
-                    output.append([nums[i], nums[l], nums[r]])
+                    output.append([nums[i],nums[l],nums[r]])
                     l+=1
                     r-=1
                     while l<r and nums[l] == nums[l-1]:
@@ -26,7 +24,4 @@ class Solution:
                 else:
                     r-=1
         return output
-
-
-
         
